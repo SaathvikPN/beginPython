dc0dff3371fd69dd2b26b93fccfba4196143a393
Saathvik PN <66002135+SaathvikPN@users.noreply.github.com>
2020-09-27 10:17:25 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,12 @@
+#by default python print function adds new line character
+print("hello")
+print("world")
+
+#hello
+#world
+
+#To discard newline character
+print("hello",end='')
+print("world",end='')
+
+#helloworld
