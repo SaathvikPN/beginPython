f6033ec83462e0635d645c071f41491c14b0f7c8
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-01 22:55:11 +0530

    Update reverse-input.py

--- starter-exercise/reverse-input.py
+++ starter-exercise/reverse-input.py
@@ -7,3 +7,20 @@ def reverse_txt(parameter):
 
 msg = input("enter text\n")
 print(reverse_txt(msg))
+
+
+#For Palindrome 
+'''
+#Reverse Input
+def reverse_txt(parameter):
+    new = ''
+    for i in parameter:
+        new = i + new
+    return new
+
+msg = input("enter text\n")
+if reverse_txt(msg)==msg:
+    print(msg,"is Palindrome")
+else:
+    print(msg,": NOT a palindrome")
+'''
