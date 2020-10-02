28fbc50563852419eeee22b25f22b652acd653d2
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-02 11:25:59 +0530

    Update MaxPairwiseProduct.py
    Fixed big1 and big2 initialisation bugs.
    Given: Input is non-negative integer.
    for 0 value input, a logical error in lines 11,15

--- Algorithm-toolbox/MaxPairwiseProduct.py
+++ Algorithm-toolbox/MaxPairwiseProduct.py
@@ -5,13 +5,13 @@
 import time
 start = time.time()
 
-def MaxPairwiseProduct(list):
-    big1,big2 = 0,0
-    for i in range(0,len(list)):
+def MaxPairwiseProduct(list,l):
+    big1,big2 = -1,-1
+    for i in range(0,l):
         if list[i]>big1:
             big1 = list[i]
             n = i
-    for i in range(0,len(list)):
+    for i in range(0,l):
          if ((i!=n) and (list[i]>big2)):
              big2 = list[i]
     return (big1*big2)
@@ -20,13 +20,14 @@ n = int(input("n: "))
 nlist = []
 
 x = input("numbers: ").split()
-for i in range(len(x)):
+l = len(x)
+for i in range(l):
     nlist.append(int(x[i])) 
     
 
 
 print(nlist)
-print(MaxPairwiseProduct(nlist))
+print(MaxPairwiseProduct(nlist,l))
 
 end = time.time()
 print(f"Runtime of the program is {end - start}")
