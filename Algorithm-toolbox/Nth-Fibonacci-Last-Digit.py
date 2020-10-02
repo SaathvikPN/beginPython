c97b87aa449dbd96648ca938b07a81d7d7634760
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-02 16:04:45 +0530

    Added problem description

--- Algorithm-toolbox/Nth-Fibonacci-Last-Digit.py
+++ Algorithm-toolbox/Nth-Fibonacci-Last-Digit.py
@@ -1,4 +1,12 @@
 #Last digit of Nth Fibonacci Number
+'''
+Problem Description
+Task. Given an integer 𝑛, find the last digit of the 𝑛th Fibonacci number 𝐹𝑛 (that is, 𝐹𝑛 mod 10).
+Input Format. The input consists of a single integer 𝑛.
+Constraints. 0 ≤ 𝑛 ≤ 107
+.
+Output Format. Output the last digit of 𝐹n
+'''
 
 def Fib_last_digit(n):
     if n<=1:
