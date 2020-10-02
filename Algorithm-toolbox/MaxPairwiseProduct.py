9e4265be7334fb5d6d85a2e59c762da02b86b485
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-02 10:47:09 +0530

    Create MaxPairwiseProduct.py

---
+++ Algorithm-toolbox/MaxPairwiseProduct.py
@@ -0,0 +1,32 @@
+#MaxPairwiseProduct
+#first line = no. of items
+#second line = numbers separated by spaces
+
+import time
+start = time.time()
+
+def MaxPairwiseProduct(list):
+    big1,big2 = 0,0
+    for i in range(0,len(list)):
+        if list[i]>big1:
+            big1 = list[i]
+    for i in range(0,len(list)):
+         if ((list[i]!=big1) and (list[i]>big2)):
+             big2 = list[i]
+    return (big1*big2)
+
+n = int(input("n: "))
+nlist = []
+
+x = input("numbers: ").split()
+for i in range(len(x)):
+    nlist.append(int(x[i])) 
+    
+
+
+print(nlist)
+print(MaxPairwiseProduct(nlist))
+
+end = time.time()
+print(f"Runtime of the program is {end - start}")
+    
