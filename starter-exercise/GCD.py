dbb99a4ade13b86825e699ac7e6e54907597eba9
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-02 16:54:52 +0530

    GCD using Euclid Algorithm
    Includes both functions - Naive Method and Euclid Algorithm to calculate GCD

--- starter-exercise/GCD.py
+++ starter-exercise/GCD.py
@@ -1,4 +1,25 @@
-def gcd(a,b):
+def gcd(numbers):
+    a = numbers[0]
+    b = numbers[1]
+    smaller = a if a<b else b
+    bigger = a+b-smaller
+    if smaller==0:
+        return bigger
+    else:
+        r= -1
+        while (r!=0):
+            q = bigger // smaller
+            r = bigger % smaller
+        
+            bigger = (smaller*q)+r
+        
+            bigger = smaller
+            smaller = r
+        return bigger
+
+def gcd_naive(numbers):
+    a = numbers[0]
+    b = numbers[1]
     smaller = a if a<b else b
     if ((a==0)and(b==0)):
         return ("NOT DEFINED")
@@ -11,6 +32,14 @@ def gcd(a,b):
             smaller = smaller - 1
         return ("terminated")
 
-m = int(input("1st number "))
-n = int(input("2nd number "))
-print("GCD of {} and {} is \"{}\"".format(m,n,gcd(m,n)))
+    
+print('''GCD Calculator
+Enter two numbers separated by a space''')
+n = input().split()
+numbers = []
+for i in range(2):
+    numbers.append(int(n[i]))
+print("Using Naive Method GCD is",gcd_naive(numbers))
+print("Using Euclid Algorithm GCD is",gcd(numbers))
+
+
