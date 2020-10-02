d5c1d8908410a0a75b03d6559b8d02e37980726c
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-02 16:20:50 +0530

    Fixed Zero number issue
    GCD of "N" and "0" is "N"
    Bug : Division by zero error
    
    Included:
    GCD of 0,0 is NOT DEFINED

--- starter-exercise/GCD.py
+++ starter-exercise/GCD.py
@@ -1,12 +1,16 @@
 def gcd(a,b):
     smaller = a if a<b else b
-    while (smaller>0):
-        if((a%smaller==0) and (b%smaller==0)):
-            return smaller
-        smaller = smaller - 1
-    return ("terminated")
+    if ((a==0)and(b==0)):
+        return ("NOT DEFINED")
+    elif smaller==0:
+        return a+b-smaller
+    else:
+        while (smaller>0):
+            if((a%smaller==0) and (b%smaller==0)):
+                return smaller
+            smaller = smaller - 1
+        return ("terminated")
 
 m = int(input("1st number "))
 n = int(input("2nd number "))
 print("GCD of {} and {} is \"{}\"".format(m,n,gcd(m,n)))
-    
