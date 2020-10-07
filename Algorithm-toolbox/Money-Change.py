1d1e4a80191b42001ce06e8dab20e2be59036a89
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-07 23:55:06 +0530

    Update Money-Change.py
    Added Function time report.

--- Algorithm-toolbox/Money-Change.py
+++ Algorithm-toolbox/Money-Change.py
@@ -1,11 +1,21 @@
 ''' Money Change Question
 Convert money into min no. of coins [Denominations 1,5,10 only]
 '''
+import time
+
+n = int(input())
+
+# starting time
+start = time.time()
 
 def minCoins(n):
     a = n//10
-    b = (n - (a*10))//5
-    c = n - ((a*10)+(b*5))
+    b = (n%10)//5
+    c = (n%10)%5
     return a+b+c
-n = int(input())
+
 print(minCoins(n))
+
+# end time
+end = time.time()
+print(f"Runtime of the program is {end - start} seconds")
