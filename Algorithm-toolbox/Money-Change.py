695b9390e60a784a09dd045e96d6555bcf85bcdd
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-08 00:02:16 +0530

    Update Money-Change.py
    Enhanced output readability

--- Algorithm-toolbox/Money-Change.py
+++ Algorithm-toolbox/Money-Change.py
@@ -4,7 +4,6 @@ Convert money into min no. of coins [Denominations 1,5,10 only]
 import time
 
 n = int(input())
-
 # starting time
 start = time.time()
 
@@ -12,6 +11,10 @@ def minCoins(n):
     a = n//10
     b = (n%10)//5
     c = (n%10)%5
+    print("{1} : Rs.{0} coins ".format(10,a))
+    print("{1} : Rs.{0} coins ".format(5,b))
+    print("{1} : Rs.{0} coins ".format(1,c))
+    
     return a+b+c
 
 print(minCoins(n))
