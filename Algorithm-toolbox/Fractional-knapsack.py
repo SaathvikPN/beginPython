487cdb2a50aaa0108bb0abefe7493beafa7c368f
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2020-10-12 11:16:50 +0530

    Create Fractional-knapsack.py

---
+++ Algorithm-toolbox/Fractional-knapsack.py
@@ -0,0 +1,43 @@
+'''
+Maximum value of Loot Problem
+'''
+import sys
+def get_opt_value(capacity,values,weights):
+    value = 0
+    value_per_weight = []
+    length = len(weights) 
+    for i in range(length):
+        x = values[i]/weights[i]
+        value_per_weight.append(x)
+    while(capacity!=0):
+        chooseIndex = value_per_weight.index(max(value_per_weight))
+        putWeight = weights[chooseIndex]
+        if(putWeight>capacity):
+            excess = putWeight - capacity
+            putWeight = putWeight - excess
+            value = putWeight*value_per_weight[chooseIndex]
+            return value
+        else:
+            capacity = capacity - putWeight
+            value = value + (putWeight*value_per_weight[chooseIndex])
+            del value_per_weight[chooseIndex]
+            del weights[chooseIndex]
+            del values[chooseIndex]
+    return value
+        
+if __name__ == "__main__":
+    
+    data = list(map(int,input().split()))
+    
+    for line in range(data[0]):
+        userData = list(map(int,input().split()))
+        data = data + userData
+        
+    print("Data",data)
+    n,capacity = data[0:2]
+    values = data[2:(2*n)+2:2] #last element in range not considered
+    weights = data[3:(2*n)+3:2]
+    print("values",values)
+    print("weights",weights)
+    opt_value = get_opt_value(capacity,values,weights)
+    print("{:.4f}".format(opt_value))
